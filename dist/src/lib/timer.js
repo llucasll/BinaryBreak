7ad5a8a52ef128602a4e96f2c4ecf5c8267ffe97
Lucas Camilo <llucasll.k@hotmail.com>
2021-05-06 00:04:51 -0300

    Timer utilities better documented

--- dist/src/lib/timer.js
+++ dist/src/lib/timer.js
@@ -2,21 +2,24 @@ import { getProducer, toArray } from "./utils.js";
 
 /**
  * Measures duration of the given callback
- * @param callback
+ * @param callback sync or async
  * @return {Promise<number>} elapsed time in milliseconds
  */
-export async function timed (callback) {
+export function timed (callback) {
 	const dtime = chronometer();
-	await callback();
-	return dtime();
+	const result = callback();
+	
+	return result instanceof Promise?
+		result.then(dtime)
+		: dtime();
 }
 
 /**
- * @param ms
+ * @param duration in milliseconds
  * @return {Promise<void>} a Promise which resolves in the given milliseconds
  */
-export function sleep (ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+export function sleep (duration) {
+	return new Promise(resolve => setTimeout(resolve, duration));
 }
 
 function* chronometerGenerator () {
@@ -27,7 +30,7 @@ function* chronometerGenerator () {
 }
 
 /**
- * @return {(function(): number)} a initialized timer which returns the elapsed time since initialization
+ * @return {(function(): number)} an initialized timer which returns the elapsed time since initialization
  */
 export function chronometer () {
 	return getProducer(chronometerGenerator);
@@ -56,11 +59,17 @@ export function stepChronometer () {
 	return getProducer(stepChronometerGenerator);
 }
 
-export async function* intervalGenerator (ms, timeout = Infinity) {
+/**
+ * Yields asynchronously the elapsed time, always equals or more than given interval
+ * @param interval in milliseconds
+ * @param timeout time limit when the iterator gets dried
+ * @return {AsyncGenerator<number>}
+ */
+export async function* intervalGenerator (interval, timeout = Infinity) {
 	const dtime = chronometer();
 	
 	while (dtime() < timeout) {
-		yield await timed(_ => sleep(ms));
+		yield await timed(_ => sleep(interval));
 	}
 }
 
